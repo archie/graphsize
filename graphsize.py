fa64dfa7f98c280e727194a4c30997e847ce2158
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-09 12:32:37 +0100

    initial data and instructions

---
+++ graphsize.py
@@ -0,0 +1,17 @@
+"""
+Implement formula (1) from "Estimating Sizes of Social Networks 
+via Biased Sampling". Apply it to nodes collected by:
+(a) UIS WOR, 
+(b) UIS WR, 
+(c) WIS WR (with weights equal to node degrees), 
+(d) RW (using all nodes, or every k-th node; the weights are 
+node degrees). 
+
+What do you observe?
+"""
+
+import networkx as nx
+
+Graph = nx.read_edgelist("p2p-Gnutella31.txt",
+                         delimiter='\t',  
+                         nodetype=int)
