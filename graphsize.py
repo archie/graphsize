257126978843028e0ba8a9165f094676d9803a89
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-09 12:36:25 +0100

    more sample code

--- graphsize.py
+++ graphsize.py
@@ -11,7 +11,27 @@ What do you observe?
 """
 
 import networkx as nx
+import random
 
 Graph = nx.read_edgelist("p2p-Gnutella31.txt",
                          delimiter='\t',  
                          nodetype=int)
+
+def UIS_WR(I,n):
+    return [random.choice(I) for i in xrange(n)]
+
+def UIS_WOR(I,n):
+    return random.sample(I,n)
+
+
+def WIS_WR(I_W):
+    # From http://code.activestate.com/recipes/117241/
+    # I_W is a list of (item,weight) tuples,
+    # e.g. I_W = [(‘A', 1.25),(‘B', 2.5), (‘C’, 3.0)]
+    weight_sum = sum(weight for item,weight in I_W)
+    n = random.uniform(0, weight_sum)
+    for item, weight in I_W:
+        if n < weight:
+            break
+        n = n - weight
+    return item 
