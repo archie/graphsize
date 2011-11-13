e786f7b1c6d5b2d43cadf810a71aec2107c98495
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-13 18:32:14 +0100

    simplifies and fixes download script

--- download_data.sh
+++ download_data.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 # download and extract the data if absent
-if [ ! -f p2p-Gnutella31.txt.gz ];
-then
-    curl http://snap.stanford.edu/data/p2p-Gnutella31.txt.gz | gunzip > p2p-Gnutella31.txt
-fi
-
-# extract the data if absent
 if [ ! -f p2p-Gnutella31.txt ];
 then
-    gunzip p2p-Gnutella31.txt
+    curl http://snap.stanford.edu/data/p2p-Gnutella31.txt.gz | gunzip > p2p-Gnutella31.txt
 fi
 
 # prepare truncated data if absent
