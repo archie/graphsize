ea782a57ccb434529013bada5e53d5f24cb8931c
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-13 16:50:17 +0100

    collision counter should also raise on empty list

--- graphsize_test.py
+++ graphsize_test.py
@@ -5,6 +5,7 @@ class GraphSizeTest(unittest.TestCase):
     def test_collision_count_raises_on_no_errors(self):
         '''collision_count should raise error when no collisions'''
         self.assertRaises(Exception, graphsize.collision_count, [1, 2, 3, 4])
+        self.assertRaises(Exception, graphsize.collision_count, [])
 
 
     def test_collision_count_counts_collisions(self):
