5e378212b963b66f0dcc4989efe52d7a91de09d6
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-15 10:12:46 +0100

    adjusted for refactor

--- mhrw_sample.py
+++ mhrw_sample.py
@@ -12,7 +12,7 @@ def increasing_sample_size():
     collected = {}
     for sample_size in xrange(0, 30001, 5000):
         if sample_size == 0: continue # ugly hack to skip first
-        collected[sample_size] = run(3, graphsize.gnutella_mhrw, 
+        collected[sample_size] = run(3, mhrw.gnutella_mhrw, 
                                      {'samples': sample_size, 
                                       'length': 300,
                                       'thinning': 5})
@@ -22,7 +22,7 @@ def increasing_walk_length_sample_is_10000():
     collected = {}
     for walk in xrange(0,600,50):
         if walk == 0: continue # ugly hack to skip first
-        collected[walk] = run(3, graphsize.gnutella_mhrw, 
+        collected[walk] = run(3, mhrw.gnutella_mhrw, 
                               {'samples': 10000, 
                                'length': walk,
                                'thinning': 5})
@@ -32,7 +32,7 @@ def increasing_walk_length_sample_is_10000():
 def increasing_thinning_sample_is_10000_walk_is_100():
     collected = {}
     for thinning in xrange(10,101,10):
-        collected[thinning] = run(3, graphsize.gnutella_mhrw, 
+        collected[thinning] = run(3, mhrw.gnutella_mhrw, 
                                   {'samples': 10000, 
                                    'length': 300,
                                    'thinning': thinning})
@@ -41,13 +41,13 @@ def increasing_thinning_sample_is_10000_walk_is_100():
 
 if '__main__' == __name__:
     sample_size = increasing_sample_size()
-    plotter.print_data("sample_size.data", sample_size)
+    plotter.print_data("mhrw_size.data", sample_size)
 
     walk = increasing_walk_length_sample_is_10000()
-    plotter.print_data("walk.data", walk)
+    plotter.print_data("mhrw_walk.data", walk)
 
     thinning = increasing_thinning_sample_is_10000_walk_is_100()
-    plotter.print_data("thinning.data", thinning)
+    plotter.print_data("mhrw_thinning.data", thinning)
 
 #    print '\n\nSample size: ', sample_size
 #    print '\nWalk: ', walk
