34916e5058c010711b6a4e876cb29fea8e959f7e
Nicholas Rutherford <nick.rutherford@gmail.com>
2011-11-15 02:46:36 +0100

    fixes uis_wr run on the gnutella data :D

--- uis_wr.py
+++ uis_wr.py
@@ -6,24 +6,7 @@ def UIS_WR(seq, n):
     """returns n random elements from seq with replacement"""
     return [random.choice(seq) for i in xrange(n)]
 
-def gnutella_truncated():
-    """a reduced-size graph example"""
-    graph = nx.read_edgelist("p2p-Gnutella31.truncated.txt", delimiter='\t', nodetype=int)
-    print "Running truncated Gnutella size estimate"
-    graph_size = graph.number_of_nodes()
-    samples = graph_size * 8
-    print 'original size', graph_size
-    print 'Estimated graph size ({0}): '.format(samples), estimate_size(graph, n_samples = samples)
-
-def gnutella():
-    graph = nx.read_edgelist("p2p-Gnutella31.txt", delimiter='\t', nodetype=int)
-    print "Running extended Gnutella size estimate"
-    graph_size = graph.number_of_nodes()
-    samples = graph_size * 2
-    print 'original size', graph_size
-    print 'Estimated graph size ({0}): '.format(samples), estimate_size(graph, n_samples = samples)
-
-def estimate_size(graph, n_samples=-1):
+def uis_wr_size_estimate(graph, n_samples=-1):
     # determine the number of samples
     if n_samples == -1: n_samples = graph.size() * 2
 
@@ -34,3 +17,10 @@ def estimate_size(graph, n_samples=-1):
     collisions = graphsize.collision_count(node_samples)
     return graphsize.estimate_size(sum_of_degrees, sum_of_inverse_degrees, collisions)
 
+if __name__ == "__main__":    
+    graph = nx.read_edgelist("p2p-Gnutella31.txt", delimiter='\t', nodetype=int)
+    print "Running extended Gnutella size estimate"
+    samples = 10000
+    print 'original size', graph.number_of_nodes()
+    print 'Estimated graph size ({0} samples): '.format(samples)
+    print uis_wr_size_estimate(graph, n_samples = samples)
