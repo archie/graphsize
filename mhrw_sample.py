5ca377aed1d0a69382ee43d62052a9b9b40fbd88
Marcus Ljungblad <marcus@ljungblad.nu>
2011-11-15 17:15:21 +0100

    some notes on where to start

--- mhrw_sample.py
+++ mhrw_sample.py
@@ -39,12 +39,8 @@ if '__main__' == __name__:
     sample_size = increasing_sample_size()
     plotter.print_data("mhrw_size.data", sample_size)
 
-#    walk = increasing_walk_length_sample_is_10000()
-#    plotter.print_data("mhrw_walk.data", walk)
+    walk = increasing_walk_length_sample_is_10000()
+    plotter.print_data("mhrw_walk.data", walk)
 
-#    thinning = increasing_thinning_sample_is_10000_walk_is_100()
-#    plotter.print_data("mhrw_thinning_2.data", thinning)
-
-#    print '\n\nSample size: ', sample_size
-#    print '\nWalk: ', walk
-#    print '\Thinning: ', thinning
+    thinning = increasing_thinning_sample_is_10000_walk_is_100()
+    plotter.print_data("mhrw_thinning.data", thinning)
